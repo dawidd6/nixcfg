8b6d1b2819ceda6939ff27255cca7bd641130272
Dawid Dziurla <dawidd0811@gmail.com>
2023-05-02 13:44:34 +0200

    nixos: update

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -2,11 +2,13 @@
   imports = [
     ./hardware-configuration.nix
   ];
+
   nixpkgs = {
     config = {
       allowUnfree = true;
     };
   };
+
   nix = {
     registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
     nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
@@ -15,7 +17,9 @@
       auto-optimise-store = true;
     };
   };
+
   boot = {
+    cleanTmpDir = true;
     loader = {
       systemd-boot = {
         enable = true;
@@ -25,8 +29,8 @@
         efiSysMountPoint = "/boot/efi";
       };
     };
-    cleanTmpDir = true;
   };
+
   networking = {
     hostName = "zotac";
     networkmanager = {
@@ -36,38 +40,43 @@
       };
     };
   };
+
   time = {
     timeZone = "Europe/Warsaw";
   };
+
   i18n = {
     defaultLocale = "en_GB.UTF-8";
   };
+
   users = {
     users = {
       dawidd6 = {
-        isNormalUser = true;
         description = "dawidd6";
-        extraGroups = [
-	  "networkmanager"
-	  "wheel"
-	];
         initialPassword = "dawidd6";
-	openssh = {
-	  authorizedKeys = {
-	    keys = [
-	      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCjPWvOxxVyoaF+sZV4Q32mzU5IR+2OKvT+czFwICMNhBq99V88sMH0V+LOs4YwhvameikF502GANoqNVbqiZgfc2uxV4I/9dvTXo3REx+sB9MNasjiUcbz8cdR7zIb1IWBo19VclRSJDE9UFeGJWL72l4OVEvzocbgsDGStjxPo6kILh0f6wXTJr9XRFxZzA0BeQU3D15qIImxKwxQb21aMaPxhRkEgObQweN30LL6rAtenCCJXwpvjsBoJNTE7ZHo8Mlh7lIkXdZxcb7h7jeK3wvkL2BUrwyBbBMf0rk7rAN73Ei3rqF1qDGENNng20j0CRpFaq/cyUcM9b6rTS+7 dawidd6"
+        isNormalUser = true;
+        extraGroups = [
+	      "networkmanager"
+	      "wheel"
 	    ];
-	  };
-	};
+	    openssh = {
+	      authorizedKeys = {
+	        keys = [
+	          "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCjPWvOxxVyoaF+sZV4Q32mzU5IR+2OKvT+czFwICMNhBq99V88sMH0V+LOs4YwhvameikF502GANoqNVbqiZgfc2uxV4I/9dvTXo3REx+sB9MNasjiUcbz8cdR7zIb1IWBo19VclRSJDE9UFeGJWL72l4OVEvzocbgsDGStjxPo6kILh0f6wXTJr9XRFxZzA0BeQU3D15qIImxKwxQb21aMaPxhRkEgObQweN30LL6rAtenCCJXwpvjsBoJNTE7ZHo8Mlh7lIkXdZxcb7h7jeK3wvkL2BUrwyBbBMf0rk7rAN73Ei3rqF1qDGENNng20j0CRpFaq/cyUcM9b6rTS+7 dawidd6"
+	        ];
+	      };
+	    };
       };
     };
   };
+
   environment = {
     systemPackages = with pkgs; [
       htop
       lm_sensors
     ];
   };
+
   programs = {
     neovim = {
       enable = true;
@@ -79,16 +88,19 @@
       enable = true;
     };
   };
+
   services = {
     openssh = {
       enable = true;
     };
   };
+
   virtualisation = {
     podman = {
       enable = true;
     };
   };
+
   system = {
     stateVersion = "22.11";
   };
--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -2,25 +2,29 @@
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
+
   boot = {
     initrd = {
       availableKernelModules = [ "ahci" "xhci_pci" "usbhid" "ums_realtek" "usb_storage" "sd_mod" "sdhci_pci" ];
     };
     kernelModules = [ "kvm-intel" ];
   };
+
   fileSystems = {
     "/" = {
-      device = "/dev/disk/by-uuid/76fee5e5-78de-4203-af82-0dc3be4ad74b";
+      device = "/dev/mmcblk0p2";
       fsType = "ext4";
     };
     "/boot/efi" = {
-      device = "/dev/disk/by-uuid/404E-91CE";
+      device = "/dev/mmcblk0p1";
       fsType = "vfat";
     };
   };
+
   nixpkgs = {
     hostPlatform = lib.mkDefault "x86_64-linux";
   };
+
   hardware = {
     cpu = {
       intel = {
