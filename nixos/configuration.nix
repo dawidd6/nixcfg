40d8e6e0c986d66cfefb7fa875c6aad9a7c5779b
Dawid Dziurla <dawidd0811@gmail.com>
2023-05-02 13:41:06 +0200

    nixos: change style

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -2,9 +2,11 @@
   imports = [
     ./hardware-configuration.nix
   ];
-
-  nixpkgs.config.allowUnfree = true;
-
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+    };
+  };
   nix = {
     registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
     nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
@@ -13,45 +15,81 @@
       auto-optimise-store = true;
     };
   };
-
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  boot.cleanTmpDir = true;
-
-  networking.hostName = "zotac";
-  networking.networkmanager.enable = true;
-  networking.networkmanager.wifi.powersave = false;
-
-  time.timeZone = "Europe/Warsaw";
-
-  i18n.defaultLocale = "en_GB.UTF-8";
-
-  users.users.dawidd6 = {
-    isNormalUser = true;
-    description = "dawidd6";
-    extraGroups = [ "networkmanager" "wheel" ];
-    initialPassword = "dawidd6";
+  boot = {
+    loader = {
+      systemd-boot = {
+        enable = true;
+      };
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot/efi";
+      };
+    };
+    cleanTmpDir = true;
   };
-
-  environment.systemPackages = with pkgs; [
-    htop
-    lm_sensors
-  ];
-
-  programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-    viAlias = true;
-    vimAlias = true;
+  networking = {
+    hostName = "zotac";
+    networkmanager = {
+      enable = true;
+      wifi = {
+        powersave = false;
+      };
+    };
   };
-  programs.git = {
-    enable = true;
+  time = {
+    timeZone = "Europe/Warsaw";
   };
-
-  services.openssh.enable = true;
-
-  virtualisation.podman.enable = true;
-
-  system.stateVersion = "22.11";
+  i18n = {
+    defaultLocale = "en_GB.UTF-8";
+  };
+  users = {
+    users = {
+      dawidd6 = {
+        isNormalUser = true;
+        description = "dawidd6";
+        extraGroups = [
+	  "networkmanager"
+	  "wheel"
+	];
+        initialPassword = "dawidd6";
+	openssh = {
+	  authorizedKeys = {
+	    keys = [
+	      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCjPWvOxxVyoaF+sZV4Q32mzU5IR+2OKvT+czFwICMNhBq99V88sMH0V+LOs4YwhvameikF502GANoqNVbqiZgfc2uxV4I/9dvTXo3REx+sB9MNasjiUcbz8cdR7zIb1IWBo19VclRSJDE9UFeGJWL72l4OVEvzocbgsDGStjxPo6kILh0f6wXTJr9XRFxZzA0BeQU3D15qIImxKwxQb21aMaPxhRkEgObQweN30LL6rAtenCCJXwpvjsBoJNTE7ZHo8Mlh7lIkXdZxcb7h7jeK3wvkL2BUrwyBbBMf0rk7rAN73Ei3rqF1qDGENNng20j0CRpFaq/cyUcM9b6rTS+7 dawidd6"
+	    ];
+	  };
+	};
+      };
+    };
+  };
+  environment = {
+    systemPackages = with pkgs; [
+      htop
+      lm_sensors
+    ];
+  };
+  programs = {
+    neovim = {
+      enable = true;
+      defaultEditor = true;
+      viAlias = true;
+      vimAlias = true;
+    };
+    git = {
+      enable = true;
+    };
+  };
+  services = {
+    openssh = {
+      enable = true;
+    };
+  };
+  virtualisation = {
+    podman = {
+      enable = true;
+    };
+  };
+  system = {
+    stateVersion = "22.11";
+  };
 }
