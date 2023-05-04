dcf0063b1790f3e1173ccaffa0e9597208e2df5a
Dawid Dziurla <dawidd0811@gmail.com>
2023-05-04 09:49:21 +0200

    nixos: change style again

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,108 +1,47 @@
 { inputs, lib, config, pkgs, ... }: {
-  imports = [
-    ./hardware-configuration.nix
+  imports = [ ./hardware-configuration.nix ];
+
+  nixpkgs.config.allowUnfree = true;
+
+  nix.registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
+  nix.nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
+  nix.settings.experimental-features = "nix-command flakes";
+  nix.settings.auto-optimise-store = true;
+
+  boot.cleanTmpDir = true;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+
+  networking.hostName = "zotac";
+  networking.networkmanager.enable = true;
+  networking.networkmanager.wifi.powersave = false;
+
+  time.timeZone = "Europe/Warsaw";
+
+  i18n.defaultLocale = "en_GB.UTF-8";
+
+  users.users.dawidd6.description = "dawidd6";
+  users.users.dawidd6.initialPassword = "dawidd6";
+  users.users.dawidd6.isNormalUser = true;
+  users.users.dawidd6.extraGroups = [ "networkmanager" "wheel" ];
+  users.users.dawidd6.openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCjPWvOxxVyoaF+sZV4Q32mzU5IR+2OKvT+czFwICMNhBq99V88sMH0V+LOs4YwhvameikF502GANoqNVbqiZgfc2uxV4I/9dvTXo3REx+sB9MNasjiUcbz8cdR7zIb1IWBo19VclRSJDE9UFeGJWL72l4OVEvzocbgsDGStjxPo6kILh0f6wXTJr9XRFxZzA0BeQU3D15qIImxKwxQb21aMaPxhRkEgObQweN30LL6rAtenCCJXwpvjsBoJNTE7ZHo8Mlh7lIkXdZxcb7h7jeK3wvkL2BUrwyBbBMf0rk7rAN73Ei3rqF1qDGENNng20j0CRpFaq/cyUcM9b6rTS+7 dawidd6" ];
+
+  environment.systemPackages = with pkgs; [
+    borgbackup
+    htop
+    lm_sensors
   ];
 
-  nixpkgs = {
-    config = {
-      allowUnfree = true;
-    };
-  };
+  programs.neovim.enable = true;
+  programs.neovim.defaultEditor = true;
+  programs.neovim.viAlias = true;
+  programs.neovim.vimAlias = true;
+  programs.git.enable = true;
 
-  nix = {
-    registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
-    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
-    settings = {
-      experimental-features = "nix-command flakes";
-      auto-optimise-store = true;
-    };
-  };
+  services.openssh.enable = true;
 
-  boot = {
-    cleanTmpDir = true;
-    loader = {
-      systemd-boot = {
-        enable = true;
-      };
-      efi = {
-        canTouchEfiVariables = true;
-        efiSysMountPoint = "/boot/efi";
-      };
-    };
-  };
+  virtualisation.podman.enable = true;
 
-  networking = {
-    hostName = "zotac";
-    networkmanager = {
-      enable = true;
-      wifi = {
-        powersave = false;
-      };
-    };
-  };
-
-  time = {
-    timeZone = "Europe/Warsaw";
-  };
-
-  i18n = {
-    defaultLocale = "en_GB.UTF-8";
-  };
-
-  users = {
-    users = {
-      dawidd6 = {
-        description = "dawidd6";
-        initialPassword = "dawidd6";
-        isNormalUser = true;
-        extraGroups = [
-	      "networkmanager"
-	      "wheel"
-	    ];
-	    openssh = {
-	      authorizedKeys = {
-	        keys = [
-	          "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCjPWvOxxVyoaF+sZV4Q32mzU5IR+2OKvT+czFwICMNhBq99V88sMH0V+LOs4YwhvameikF502GANoqNVbqiZgfc2uxV4I/9dvTXo3REx+sB9MNasjiUcbz8cdR7zIb1IWBo19VclRSJDE9UFeGJWL72l4OVEvzocbgsDGStjxPo6kILh0f6wXTJr9XRFxZzA0BeQU3D15qIImxKwxQb21aMaPxhRkEgObQweN30LL6rAtenCCJXwpvjsBoJNTE7ZHo8Mlh7lIkXdZxcb7h7jeK3wvkL2BUrwyBbBMf0rk7rAN73Ei3rqF1qDGENNng20j0CRpFaq/cyUcM9b6rTS+7 dawidd6"
-	        ];
-	      };
-	    };
-      };
-    };
-  };
-
-  environment = {
-    systemPackages = with pkgs; [
-      borgbackup
-      htop
-      lm_sensors
-    ];
-  };
-
-  programs = {
-    neovim = {
-      enable = true;
-      defaultEditor = true;
-      viAlias = true;
-      vimAlias = true;
-    };
-    git = {
-      enable = true;
-    };
-  };
-
-  services = {
-    openssh = {
-      enable = true;
-    };
-  };
-
-  virtualisation = {
-    podman = {
-      enable = true;
-    };
-  };
-
-  system = {
-    stateVersion = "22.11";
-  };
+  system.stateVersion = "22.11";
 }
--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -1,42 +1,18 @@
 { config, lib, pkgs, modulesPath, ... }: {
-  imports = [
-    (modulesPath + "/installer/scan/not-detected.nix")
-  ];
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
-  boot = {
-    initrd = {
-      availableKernelModules = [ "ahci" "xhci_pci" "usbhid" "ums_realtek" "usb_storage" "sd_mod" "sdhci_pci" ];
-    };
-    kernelModules = [ "kvm-intel" ];
-  };
+  boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "usbhid" "ums_realtek" "usb_storage" "sd_mod" "sdhci_pci" ];
+  boot.kernelModules = [ "kvm-intel" ];
 
-  fileSystems = {
-    "/" = {
-      device = "/dev/mmcblk0p2";
-      fsType = "ext4";
-    };
-    "/boot/efi" = {
-      device = "/dev/mmcblk0p1";
-      fsType = "vfat";
-    };
-    "/home/dawidd6/Backups" = {
-      device = "/dev/disk/by-label/Backups";
-      fsType = "ext4";
-      options = [
-        "nofail"
-      ];
-    };
-  };
+  fileSystems."/".device = "/dev/mmcblk0p2";
+  fileSystems."/".fsType = "ext4";
+  fileSystems."/boot/efi".device = "/dev/mmcblk0p1";
+  fileSystems."/boot/efi".fsType = "vfat";
+  fileSystems."/home/dawidd6/Backups".device = "/dev/disk/by-label/Backups";
+  fileSystems."/home/dawidd6/Backups".fsType = "ext4";
+  fileSystems."/home/dawidd6/Backups".options = [ "nofail" ];
 
-  nixpkgs = {
-    hostPlatform = lib.mkDefault "x86_64-linux";
-  };
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 
-  hardware = {
-    cpu = {
-      intel = {
-        updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-      };
-    };
-  };
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
