9f826a9f8e50b4eef5c800ec634589d0131b78db
Dawid Dziurla <dawidd0811@gmail.com>
2023-05-08 16:40:35 +0200

    refactor (#3)

---
+++ home-manager/default.nix
@@ -0,0 +1,41 @@
+{ inputs, config, pkgs, ... }: {
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+      allowUnfreePredicate = (_: true);
+    };
+  };
+
+  home = {
+    stateVersion = "22.11";
+    username = "dawidd6";
+    homeDirectory = "/home/dawidd6";
+    packages = [
+      pkgs.htop
+      pkgs.ghorg
+      pkgs.podman
+      pkgs.spotify
+      pkgs.keepassxc
+      pkgs.google-chrome
+      pkgs.vscode
+      pkgs.copyq
+      pkgs.vorta
+    ];
+  };
+
+  programs = {
+    neovim = {
+      enable = true;
+      defaultEditor = true;
+      viAlias = true;
+      vimAlias = true;
+    };
+    home-manager.enable = true;
+    fish.enable = true;
+  };
+
+  xdg.configFile = {
+    "containers/policy.json".text = "{\"default\":[{\"type\":\"insecureAcceptAnything\"}]}";
+    "containers/registries.conf".text = "unqualified-search-registries=[\"docker.io\"]";
+  };
+}
