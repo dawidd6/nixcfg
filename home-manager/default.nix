52a6fb19b65838f5951d4a42ba5d60947088617d
Dawid Dziurla <dawidd0811@gmail.com>
2023-05-09 12:42:23 +0200

    home: merge

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -1,4 +1,4 @@
-{ inputs, config, pkgs, ... }: {
+{ config, pkgs, ... }: {
   nixpkgs = {
     config = {
       allowUnfree = true;
@@ -6,36 +6,70 @@
     };
   };
 
-  home = {
-    stateVersion = "22.11";
-    username = "dawidd6";
-    homeDirectory = "/home/dawidd6";
-    packages = [
-      pkgs.htop
-      pkgs.ghorg
-      pkgs.podman
-      pkgs.spotify
-      pkgs.keepassxc
-      pkgs.google-chrome
-      pkgs.vscode
-      pkgs.copyq
-      pkgs.vorta
-    ];
-  };
+  targets.genericLinux.enable = true;
 
-  programs = {
-    neovim = {
-      enable = true;
-      #defaultEditor = true;
-      viAlias = true;
-      vimAlias = true;
-    };
-    home-manager.enable = true;
-    fish.enable = true;
-  };
+  home.stateVersion = "22.11";
+  home.username = "dawidd6";
+  home.homeDirectory = "/home/dawidd6";
+  home.packages = [
+    pkgs.ansible
+    pkgs.ansible-lint
+    pkgs.bat
+    pkgs.btop
+    pkgs.cpio
+    pkgs.curl
+    pkgs.copyq
+    pkgs.diff-so-fancy
+    pkgs.diffoscope
+    pkgs.distrobox
+    pkgs.dos2unix
+    pkgs.filezilla
+    pkgs.fzf
+    pkgs.gh
+    pkgs.ghorg
+    pkgs.gimp
+    pkgs.glab
+    pkgs.google-chrome
+    pkgs.gpick
+    pkgs.htop
+    pkgs.inkscape
+    pkgs.ipcalc
+    pkgs.jq
+    pkgs.keepassxc
+    pkgs.lazygit
+    pkgs.libvirt
+    pkgs.lm-sensors
+    pkgs.ncdu
+    pkgs.nmap
+    pkgs.pavucontrol
+    pkgs.podman
+    pkgs.qemu
+    pkgs.sshpass
+    pkgs.spotify
+    pkgs.starship
+    pkgs.strace
+    pkgs.tealdeer
+    pkgs.trash-cli
+    pkgs.tree
+    pkgs.virt-manager
+    pkgs.vorta
+    pkgs.vscode
+    pkgs.xsel
+    pkgs.zoxide
+  ];
 
-  xdg.configFile = {
-    "containers/policy.json".text = "{\"default\":[{\"type\":\"insecureAcceptAnything\"}]}";
-    "containers/registries.conf".text = "unqualified-search-registries=[\"docker.io\"]";
-  };
+  programs.neovim.enable = true;
+  programs.neovim.defaultEditor = true;
+  programs.neovim.viAlias = true;
+  programs.neovim.vimAlias = true;
+  programs.fish.enable = true;
+  programs.git.enable = true;
+  programs.home-manager.enable = true;
+
+  xdg.configFile."containers/policy.json".text = ''
+    {"default":[{"type":"insecureAcceptAnything"}]}
+  '';
+  xdg.configFile."containers/registries.conf".text = ''
+    unqualified-search-registries=["docker.io"]
+  '';
 }
