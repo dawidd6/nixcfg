157a90520abee54f032575439f0ec75e516e3c12
Dawid Dziurla <dawidd0811@gmail.com>
2023-05-09 18:42:01 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/release-22.11";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -10,9 +11,6 @@
   };
 
   outputs = { nixpkgs, home-manager, ... }@inputs: {
-    nixosModules = {
-      #test = import ./nixos/modules/test.nix
-    };
     nixosConfigurations = {
       "zotac" = nixpkgs.lib.nixosSystem {
         pkgs = nixpkgs.legacyPackages.x86_64-linux;
@@ -21,9 +19,6 @@
           ./nixos/hosts/zotac.nix
         ];
       };
-    };
-    homeManagerModules = {
-      #test = import ./home-manager/modules/test.nix
     };
     homeConfigurations = {
       "dawidd6" = home-manager.lib.homeManagerConfiguration {
