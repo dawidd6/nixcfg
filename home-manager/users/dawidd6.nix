28678cc39dd65bdf0b2ae280889de07852f9c73b
Dawid Dziurla <dawidd0811@gmail.com>
2023-05-10 15:36:42 +0200

    home: with pkgs

--- home-manager/users/dawidd6.nix
+++ home-manager/users/dawidd6.nix
@@ -26,51 +26,51 @@
   programs.git.enable = true;
   programs.home-manager.enable = true;
 
-  home.packages = [
-    pkgs.ansible
-    pkgs.ansible-lint
-    pkgs.bat
-    pkgs.btop
-    pkgs.cpio
-    pkgs.curl
-    pkgs.copyq
-    pkgs.diff-so-fancy
-    pkgs.diffoscope
-    pkgs.distrobox
-    pkgs.dos2unix
-    pkgs.filezilla
-    pkgs.fzf
-    pkgs.gh
-    pkgs.ghorg
-    pkgs.gimp
-    pkgs.glab
-    pkgs.google-chrome
-    pkgs.gpick
-    pkgs.htop
-    pkgs.inkscape
-    pkgs.ipcalc
-    pkgs.jq
-    pkgs.keepassxc
-    pkgs.lazygit
-    pkgs.libvirt
-    pkgs.lm_sensors
-    pkgs.ncdu
-    pkgs.nmap
-    pkgs.pavucontrol
-    pkgs.podman
-    pkgs.qemu
-    pkgs.sshpass
-    pkgs.spotify
-    pkgs.starship
-    pkgs.strace
-    pkgs.tealdeer
-    pkgs.trash-cli
-    pkgs.tree
-    pkgs.virt-manager
-    pkgs.vorta
-    pkgs.vscode
-    pkgs.xsel
-    pkgs.zoxide
+  home.packages = with pkgs; [
+    ansible
+    ansible-lint
+    bat
+    btop
+    cpio
+    curl
+    copyq
+    diff-so-fancy
+    diffoscope
+    distrobox
+    dos2unix
+    filezilla
+    fzf
+    gh
+    ghorg
+    gimp
+    glab
+    google-chrome
+    gpick
+    htop
+    inkscape
+    ipcalc
+    jq
+    keepassxc
+    lazygit
+    libvirt
+    lm_sensors
+    ncdu
+    nmap
+    pavucontrol
+    podman
+    qemu
+    sshpass
+    spotify
+    starship
+    strace
+    tealdeer
+    trash-cli
+    tree
+    virt-manager
+    vorta
+    vscode
+    xsel
+    zoxide
   ];
   home.username = "dawidd6";
   home.homeDirectory = "/home/dawidd6";
