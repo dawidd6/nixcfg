6d5a59c5bbec9e28af0b5fbc612c44a3d86ae4ac
Dawid Dziurla <dawidd0811@gmail.com>
2023-05-29 11:24:01 +0200

    devcontainer: install pkgs from flake

---
+++ .devcontainer/flake.nix
@@ -0,0 +1,16 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = {nixpkgs, ...}: let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in {
+    packages.x86_64-linux.default = pkgs.buildEnv {
+      name = "my-packages";
+      paths = with pkgs; [
+        alejandra
+        gitMinimal
+        nil
+      ];
+    };
+  };
+}
