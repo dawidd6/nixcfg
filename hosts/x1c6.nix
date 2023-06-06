288bc188f9b08526299f243cecd84904e9bc4d90
Dawid Dziurla <dawidd0811@gmail.com>
2023-06-06 15:30:18 +0200

    x1c6: remove nixos-hardware patch

--- hosts/x1c6.nix
+++ hosts/x1c6.nix
@@ -5,8 +5,4 @@ _: {
   };
   boot.initrd.luks.devices."luks-8dc1c733-01a8-431b-a8f7-68a51a62504f".device = "/dev/disk/by-uuid/8dc1c733-01a8-431b-a8f7-68a51a62504f";
   boot.initrd.luks.devices."luks-8dc1c733-01a8-431b-a8f7-68a51a62504f".keyFile = "/crypto_keyfile.bin";
-
-  # Patch nixos-hardware service enablement
-  # TODO: use my own fork of this repo
-  services.throttled.enable = false;
 }
