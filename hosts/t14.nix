a1682e3ce2843f1be9f35183ed1ce6f63438fc54
Dawid Dziurla <dawidd0811@gmail.com>
2023-07-19 22:38:16 +0200

    t14: disable fingerprint login
    it's buggy, can't unlock database, network manager is crazy

--- hosts/t14.nix
+++ hosts/t14.nix
@@ -12,4 +12,5 @@
   services.fprintd.enable = true;
   services.fprintd.tod.enable = true;
   services.fprintd.tod.driver = pkgs.libfprint-2-tod1-vfs0090;
+  security.pam.services.login.fprintAuth = false;
 }
