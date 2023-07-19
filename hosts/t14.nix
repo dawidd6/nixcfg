c0902179181358003fbb662463b97d5d9ca36d21
Dawid Dziurla <dawidd0811@gmail.com>
2023-07-19 20:27:14 +0200

    t14: enable fprintd

--- hosts/t14.nix
+++ hosts/t14.nix
@@ -1,4 +1,4 @@
-_: {
+{pkgs, ...}: {
   # Machine-specific boot configuration
   boot.initrd.secrets = {
     "/crypto_keyfile.bin" = null;
@@ -7,4 +7,9 @@ _: {
   # Enable swap on luks
   boot.initrd.luks.devices."luks-40a1edfa-0238-4f66-acb9-8ee70a8f03a9".device = "/dev/disk/by-uuid/40a1edfa-0238-4f66-acb9-8ee70a8f03a9";
   boot.initrd.luks.devices."luks-40a1edfa-0238-4f66-acb9-8ee70a8f03a9".keyFile = "/crypto_keyfile.bin";
+
+  # Fingerprint sensor
+  services.fprintd.enable = true;
+  services.fprintd.tod.enable = true;
+  services.fprintd.tod.driver = pkgs.libfprint-2-tod1-vfs0090;
 }
