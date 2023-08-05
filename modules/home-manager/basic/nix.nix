a03c72914acf714d056346030dbd5affec9b6ac7
Dawid Dziurla <dawidd0811@gmail.com>
2023-08-05 22:49:36 +0200

    nix: fix caches for user

--- modules/home-manager/basic/nix.nix
+++ modules/home-manager/basic/nix.nix
@@ -5,8 +5,8 @@
 }: {
   nix.registry = lib.mapAttrs (_: value: {flake = value;}) inputs;
 
-  nix.settings.substituters = ["https://dawidd6.cachix.org"];
-  nix.settings.trusted-public-keys = ["dawidd6.cachix.org-1:dvy2Br48mAee39Yit5P+jLLIUR3gOa1ts4w4DTJw+XQ="];
+  nix.settings.extra-substituters = ["https://dawidd6.cachix.org"];
+  nix.settings.extra-trusted-public-keys = ["dawidd6.cachix.org-1:dvy2Br48mAee39Yit5P+jLLIUR3gOa1ts4w4DTJw+XQ="];
   nix.settings.warn-dirty = false;
 
   nixpkgs.config.allowUnfree = true;
