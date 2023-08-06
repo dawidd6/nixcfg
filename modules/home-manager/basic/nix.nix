88290efded806a6861e3ea655a6ba3c8d293a400
Dawid Dziurla <dawidd0811@gmail.com>
2023-08-06 12:43:21 +0200

    nix: set nix package for hm

--- modules/home-manager/basic/nix.nix
+++ modules/home-manager/basic/nix.nix
@@ -1,10 +1,12 @@
 {
   lib,
   inputs,
+  pkgs,
   ...
 }: {
   nix.registry = lib.mapAttrs (_: value: {flake = value;}) inputs;
 
+  nix.package = pkgs.nix;
   nix.settings.extra-substituters = ["https://dawidd6.cachix.org"];
   nix.settings.extra-trusted-public-keys = ["dawidd6.cachix.org-1:dvy2Br48mAee39Yit5P+jLLIUR3gOa1ts4w4DTJw+XQ="];
   nix.settings.warn-dirty = false;
