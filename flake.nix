8e62da2f1b07de9a07904225c2d1fe2de7c9f867
Dawid Dziurla <dawidd0811@gmail.com>
2023-08-07 20:18:41 +0200

    flake: flake-parts follows nixpkgs

--- flake.nix
+++ flake.nix
@@ -7,8 +7,11 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     hardware.url = "github:nixos/nixos-hardware";
-    flake-parts.url = "github:hercules-ci/flake-parts";
     systems.url = "github:nix-systems/default-linux";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
