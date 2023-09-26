fe00ebd56b43868a2e2949821fb0916d369f75a2
Dawid Dziurla <dawidd0811@gmail.com>
2023-09-26 08:59:32 +0200

    dawid: add onedrive service

--- users/dawid.nix
+++ users/dawid.nix
@@ -1,4 +1,8 @@
-{outputs, ...}: {
+{
+  outputs,
+  pkgs,
+  ...
+}: {
   imports = [
     outputs.homeModules.basic
   ];
@@ -7,6 +11,26 @@
     outputs.overlays.additions
     outputs.overlays.modifications
   ];
+
+  home.packages = [pkgs.onedrive];
+
+  systemd.user.services.onedrive = {
+    Unit.Description = "Onedrive sync service";
+    Install.WantedBy = ["default.target"];
+    Service = {
+      Environment = [
+        "Environment=HTTP_PROXY=http://10.144.1.10:8080"
+        "Environment=HTTPS_PROXY=http://10.144.1.10:8080"
+      ];
+      Type = "simple";
+      ExecStart = ''
+        ${pkgs.onedrive}/bin/onedrive --monitor --confdir=%h/.config/onedrive
+      '';
+      Restart = "on-failure";
+      RestartSec = 3;
+      RestartPreventExitStatus = 3;
+    };
+  };
 
   home.stateVersion = "22.11";
   home.username = "dawid";
