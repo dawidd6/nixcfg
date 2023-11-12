e7287453bae1ad00ee8a6bf8a30cdc08e908b661
Dawid Dziurla <dawidd0811@gmail.com>
2023-11-12 22:15:39 +0100

    t14: fix fingerprint login

--- hosts/t14/configuration.nix
+++ hosts/t14/configuration.nix
@@ -19,8 +19,21 @@
   services.fprintd.tod.enable = true;
   services.fprintd.tod.driver = pkgs.libfprint-2-tod1-vfs0090;
 
-  #security.pam.services.login.fprintAuth = false;
-  #security.pam.services.gdm-fingerprint.fprintAuth = true;
+  security.pam.services.login.fprintAuth = false;
+  security.pam.services.gdm-fingerprint.text = ''
+    auth       required                    pam_shells.so
+    auth       requisite                   pam_nologin.so
+    auth       requisite                   pam_faillock.so      preauth
+    auth       required                    ${pkgs.fprintd}/lib/security/pam_fprintd.so
+    auth       optional                    pam_permit.so
+    auth       required                    pam_env.so
+    auth       [success=ok default=1]      ${pkgs.gnome.gdm}/lib/security/pam_gdm.so
+    auth       optional                    ${pkgs.gnome.gnome-keyring}/lib/security/pam_gnome_keyring.so
+    account    include                     login
+    password   required                    pam_deny.so
+    session    include                     login
+    session    optional                    ${pkgs.gnome.gnome-keyring}/lib/security/pam_gnome_keyring.so auto_start
+  '';
 
   boot.initrd.secrets = {
     "/crypto_keyfile.bin" = null;
