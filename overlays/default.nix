32a05f9be2518c5c1a76682c365c3ec05d104c8e
Dawid Dziurla <dawidd0811@gmail.com>
2023-11-19 21:22:11 +0100

    flake.lock: Update (#53)
    * flake.lock: Update
    
    Flake lock file updates:
    
    • Updated input 'flake-parts':
        'github:hercules-ci/flake-parts/c9afaba3dfa4085dbd2ccb38dfade5141e33d9d4' (2023-10-03)
      → 'github:hercules-ci/flake-parts/8c9fa2545007b49a5db5f650ae91f227672c3877' (2023-11-01)
    • Updated input 'hardware':
        'github:nixos/nixos-hardware/72d53d51704295f1645d20384cd13aecc182f624' (2023-10-19)
      → 'github:nixos/nixos-hardware/1721da31f9b30cbf4460c4ec5068b3b6174a4694' (2023-11-18)
    • Updated input 'home-manager':
        'github:nix-community/home-manager/81ab14626273ca38cba947d9a989c9d72b5e7593' (2023-10-21)
      → 'github:nix-community/home-manager/ecd0a800f716b80a6eac58a7ac34d6d33e6fa5ee' (2023-11-17)
    • Updated input 'nixpkgs':
        'github:nixos/nixpkgs/7c9cc5a6e5d38010801741ac830a3f8fd667a7a0' (2023-10-19)
      → 'github:nixos/nixpkgs/c757e9bd77b16ca2e03c89bf8bc9ecb28e0c06ad' (2023-11-17)
    • Updated input 'treefmt':
        'github:numtide/treefmt-nix/aae39f64f5ecbe89792d05eacea5cb241891292a' (2023-10-15)
      → 'github:numtide/treefmt-nix/e82f32aa7f06bbbd56d7b12186d555223dc399d1' (2023-11-12)
    
    * overlays: fix multipass build
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- overlays/default.nix
+++ overlays/default.nix
@@ -5,5 +5,13 @@
     ansible = prev.ansible.overrideAttrs (oldAttrs: {
       propagatedBuildInputs = oldAttrs.propagatedBuildInputs ++ [final.python3Packages.jmespath];
     });
+    multipass = prev.multipass.overrideAttrs (oldAttrs: {
+      src = prev.fetchFromGitHub {
+        inherit (oldAttrs.src) owner repo rev;
+        hash = "sha256-fy8i6L0xo+Q1unvhOb0jnQL6P84lxKZ1OGvoTOVJHHk=";
+        leaveDotGit = true;
+        fetchSubmodules = true;
+      };
+    });
   };
 }
