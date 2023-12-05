3677025fe27aa7744d529fb0cb468880dc2dbabe
Dawid Dziurla <dawidd0811@gmail.com>
2023-12-05 11:27:48 +0100

    flake: switch to 23.11 from unstable

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     hardware.url = "github:nixos/nixos-hardware";
     systems.url = "github:nix-systems/default-linux";
     flake-parts = {
@@ -13,7 +13,7 @@
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     treefmt = {
