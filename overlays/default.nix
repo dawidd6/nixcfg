c08ce29b149c3bb7d3ad69d301cdcca93ddca665
Dawid Dziurla <dawidd0811@gmail.com>
2023-12-05 11:37:11 +0100

    overlays: add unstable-packages
    for example: pkgs.unstable.htop

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,9 +1,17 @@
-{
+{inputs, ...}: {
   additions = final: _prev: import ../pkgs {pkgs = final;};
   modifications = final: prev: {
     # https://github.com/NixOS/nixpkgs/pull/173364
     ansible = prev.ansible.overrideAttrs (oldAttrs: {
       propagatedBuildInputs = oldAttrs.propagatedBuildInputs ++ [final.python3Packages.jmespath];
     });
+  };
+  # When applied, the unstable nixpkgs set (declared in the flake inputs) will
+  # be accessible through "pkgs.unstable"
+  unstable-packages = final: _prev: {
+    unstable = import inputs.nixpkgs-unstable {
+      inherit (final) system;
+      config.allowUnfree = true;
+    };
   };
 }
