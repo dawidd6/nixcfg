6e5bcf61730de0c0b55d324126a375d77fbca469
Dawid Dziurla <dawidd0811@gmail.com>
2023-12-05 11:28:20 +0100

    overlays: remove multipass

--- overlays/default.nix
+++ overlays/default.nix
@@ -5,13 +5,5 @@
     ansible = prev.ansible.overrideAttrs (oldAttrs: {
       propagatedBuildInputs = oldAttrs.propagatedBuildInputs ++ [final.python3Packages.jmespath];
     });
-    multipass = prev.multipass.overrideAttrs (oldAttrs: {
-      src = prev.fetchFromGitHub {
-        inherit (oldAttrs.src) owner repo rev;
-        hash = "sha256-fy8i6L0xo+Q1unvhOb0jnQL6P84lxKZ1OGvoTOVJHHk=";
-        leaveDotGit = true;
-        fetchSubmodules = true;
-      };
-    });
   };
 }
