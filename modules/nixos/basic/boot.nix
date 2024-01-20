a4afa7565675522739f4040b66a3353899318ac7
Dawid Dziurla <dawidd0811@gmail.com>
2024-01-20 13:29:02 +0100

    boot: up configurationLimit from 3 to 5

--- modules/nixos/basic/boot.nix
+++ modules/nixos/basic/boot.nix
@@ -6,7 +6,7 @@
   boot.tmp.cleanOnBoot = true;
 
   boot.loader.systemd-boot.enable = true;
-  boot.loader.systemd-boot.configurationLimit = 3;
+  boot.loader.systemd-boot.configurationLimit = 5;
 
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = lib.mkIf (lib.versionOlder config.system.stateVersion "23.05") "/boot/efi";
