9c940a44a5d2dfaf07fcb3bf1a62465d5ac8a4f3
Dawid Dziurla <dawidd0811@gmail.com>
2024-02-04 18:58:45 +0100

    theme: disable Yaru for now

--- modules/home-manager/graphical/theme.nix
+++ modules/home-manager/graphical/theme.nix
@@ -1,18 +1,18 @@
-{pkgs, ...}: {
-  home.packages = [pkgs.yaru-theme];
+_: {
+  #home.packages = [pkgs.yaru-theme];
 
   dconf.settings = {
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
-      cursor-theme = "Yaru";
-      icon-theme = "Yaru";
-      gtk-theme = "Yaru-dark";
+      #cursor-theme = "Yaru";
+      #icon-theme = "Yaru";
+      #gtk-theme = "Yaru-dark";
     };
-    "org/gnome/desktop/sound" = {
-      theme-name = "Yaru";
-    };
-    "org/gnome/shell/extensions/user-theme" = {
-      name = "Yaru-dark";
-    };
+    #"org/gnome/desktop/sound" = {
+    #  theme-name = "Yaru";
+    #};
+    #"org/gnome/shell/extensions/user-theme" = {
+    #  name = "Yaru-dark";
+    #};
   };
 }
