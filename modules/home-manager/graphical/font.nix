d6913d90a98c940d1c0974ffdd7ab9119871b941
Dawid Dziurla <dawidd0811@gmail.com>
2024-02-07 16:15:20 +0100

    font: ubuntu mono nerd

--- modules/home-manager/graphical/font.nix
+++ modules/home-manager/graphical/font.nix
@@ -1,13 +1,16 @@
 {pkgs, ...}: {
   fonts.fontconfig.enable = true;
 
-  home.packages = [pkgs.ubuntu-font-family];
+  home.packages = with pkgs; [
+    ubuntu-font-family
+    (nerdfonts.override { fonts = [ "Ubuntu" "UbuntuMono" ]; })
+  ];
 
   dconf.settings = {
     "org/gnome/desktop/interface" = {
       font-name = "Ubuntu 11";
       document-font-name = "FreeSans 11";
-      monospace-font-name = "Ubuntu Mono 13";
+      monospace-font-name = "UbuntuMono Nerd Font Regular 13";
     };
     "org/gnome/desktop/wm/preferences" = {
       titlebar-font = "Ubuntu Bold 11";
