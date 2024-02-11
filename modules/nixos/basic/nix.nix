a21f66d74df0d97157ffaad9fada018ab535f7b8
Dawid Dziurla <dawidd0811@gmail.com>
2024-02-11 11:22:10 +0100

    nix: simplify diff script

--- modules/nixos/basic/nix.nix
+++ modules/nixos/basic/nix.nix
@@ -42,9 +42,7 @@
     supportsDryActivation = true;
     text = ''
       if [[ -e /run/current-system ]]; then
-        echo "--- diff to current-system"
         ${pkgs.nvd}/bin/nvd --nix-bin-dir=${config.nix.package}/bin diff /run/current-system "$systemConfig"
-        echo "---"
       fi
     '';
   };
