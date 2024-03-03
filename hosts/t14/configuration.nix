94c3f42d87f6b35e7dac9b883107e5cbe3e331ab
Dawid Dziurla <dawidd0811@gmail.com>
2024-03-03 10:24:35 +0100

    t14: disable fprint login

--- hosts/t14/configuration.nix
+++ hosts/t14/configuration.nix
@@ -19,6 +19,8 @@
   services.fprintd.tod.enable = true;
   services.fprintd.tod.driver = pkgs.libfprint-2-tod1-vfs0090;
 
+  security.pam.services.login.fprintAuth = false;
+
   boot.binfmt.emulatedSystems = ["armv7l-linux" "aarch64-linux"];
 
   system.stateVersion = "23.11";
