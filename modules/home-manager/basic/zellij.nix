b976c00ab805c3ecdf89e9fcf2ebe91089f059e7
Dawid Dziurla <dawidd0811@gmail.com>
2024-04-29 11:34:32 +0200

    basic: add zellij

---
+++ modules/home-manager/basic/zellij.nix
@@ -0,0 +1,9 @@
+_: {
+  programs.zellij.enable = true;
+  programs.zellij.settings = {
+    copy_command = "xsel --clipboard";
+    copy_on_select = true;
+    default_shell = "fish";
+    pane_frames = false;
+  };
+}
