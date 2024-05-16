b3c2d7de4ff1a2db4b1a3d5f1486729b763b7ee9
Dawid Dziurla <dawidd0811@gmail.com>
2024-05-16 09:04:40 +0200

    zellij: mouse mode false

--- modules/home-manager/basic/zellij.nix
+++ modules/home-manager/basic/zellij.nix
@@ -1,9 +1,10 @@
 _: {
   programs.zellij.enable = true;
   programs.zellij.settings = {
-    copy_command = "xsel --clipboard";
-    copy_on_select = true;
+    #copy_command = "xsel --clipboard";
+    #copy_on_select = true;
     default_shell = "fish";
     pane_frames = false;
+    mouse_mode = false;
   };
 }
