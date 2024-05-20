f8cb3c5a6010703b13b19a1429f5eef70227877d
Dawid Dziurla <dawidd0811@gmail.com>
2024-05-20 20:21:03 +0200

    extensions: use clipboard-indicator

--- modules/home-manager/graphical/extensions.nix
+++ modules/home-manager/graphical/extensions.nix
@@ -1,8 +1,8 @@
 {pkgs, ...}: let
   extensions = with pkgs.gnomeExtensions; [
     appindicator
+    clipboard-indicator
     gtk-title-bar
-    pano
   ];
 in {
   home.packages = extensions;
@@ -11,16 +11,6 @@ in {
     "org/gnome/shell" = {
       enabled-extensions = map (extension: extension.extensionUuid) extensions;
       disabled-extensions = [];
-    };
-    "org/gnome/shell/extensions/pano" = {
-      history-length = 50;
-      keep-search-entry = false;
-      link-previews = false;
-      open-links-in-browser = false;
-      paste-on-select = false;
-      play-audio-on-copy = false;
-      send-notification-on-copy = false;
-      watch-exclusion-list = false;
     };
   };
 }
