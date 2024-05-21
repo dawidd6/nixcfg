5ca4260b68a491290f020a6080bb83c9064ff390
Dawid Dziurla <dawidd0811@gmail.com>
2024-05-21 13:35:59 +0200

    hosts: simplify

--- hosts/default.nix
+++ hosts/default.nix
@@ -4,10 +4,10 @@
   lib,
   ...
 }: let
-  mkNixos = hostname:
+  mkNixOS = hostname:
     inputs.nixpkgs.lib.nixosSystem {
       specialArgs = {inherit inputs outputs;};
-      modules = [(./. + "/${hostname}/configuration.nix")];
+      modules = [./${hostname}/configuration.nix];
     };
 in
-  lib.genAttrs (builtins.attrNames (lib.filterAttrs (_n: v: v == "directory") (builtins.readDir ./.))) mkNixos
+  lib.genAttrs (builtins.attrNames (lib.filterAttrs (_n: v: v == "directory") (builtins.readDir ./.))) mkNixOS
