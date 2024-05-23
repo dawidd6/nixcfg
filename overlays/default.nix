581b1655d306a4226be05cbab0b74be1f40b8d0f
Dawid Dziurla <dawidd0811@gmail.com>
2024-05-23 10:36:01 +0200

    overlays: fix unstable-packages

--- overlays/default.nix
+++ overlays/default.nix
@@ -5,10 +5,8 @@
     ansible = prev.ansible.overrideAttrs (oldAttrs: {
       propagatedBuildInputs = oldAttrs.propagatedBuildInputs ++ [final.python3Packages.jmespath];
     });
-  };
-  # When applied, the unstable nixpkgs set (declared in the flake inputs) will
-  # be accessible through "pkgs.unstable"
-  unstable-packages = final: _prev: {
+    # When applied, the unstable nixpkgs set (declared in the flake inputs)
+    # will be accessible through "pkgs.unstable" attrset
     unstable = import inputs.nixpkgs-unstable {
       inherit (final) system;
       config.allowUnfree = true;
