0101fea26cc81109bb1c96747f3efbc58a0da03f
Dawid Dziurla <dawidd0811@gmail.com>
2024-05-23 14:43:42 +0200

    neovim: switch to nixvim

--- modules/home-manager/basic/neovim.nix
+++ modules/home-manager/basic/neovim.nix
@@ -1,46 +1,97 @@
-{pkgs, ...}: {
-  programs.neovim = {
+{
+  pkgs,
+  inputs,
+  ...
+}: {
+  imports = [
+    inputs.nixvim.homeManagerModules.nixvim
+  ];
+
+  programs.nixvim = {
     enable = true;
+    enableMan = false;
     defaultEditor = true;
     viAlias = true;
     vimAlias = true;
-    plugins = with pkgs.unstable.vimPlugins; [
-      # Appearance
-      bufferline-nvim
-      lualine-nvim
-      vscode-nvim
-      # SCM helpers
-      git-blame-nvim
-      git-conflict-nvim
-      gitsigns-nvim
-      # Misc
-      mini-nvim
+    clipboard.register = "unnamedplus";
+    # TODO: after 24.05
+    colorscheme = "vscode";
+    #colorschemes.vscode.package = pkgs.unstable.vimPlugins.vscode-nvim;
+    #colorschemes.vscode.enable = true;
+    plugins = {
+      bufferline.enable = true;
+      lualine.enable = true;
+      gitblame.enable = true;
+      gitsigns.enable = true;
+      mini.enable = true;
+      mini.modules = {
+        animate = {};
+        basics = {};
+        pairs = {};
+        trailspace = {};
+      };
+    };
+    extraPlugins = [
+      {
+        # TODO: https://github.com/nix-community/nixvim/issues/1553
+        plugin = pkgs.vimPlugins.git-conflict-nvim;
+        config = ''
+          lua << EOF
+          require('git-conflict').setup()
+          EOF
+        '';
+      }
+      {
+        # TODO: replace with module after 24.05
+        plugin = pkgs.unstable.vimPlugins.vscode-nvim;
+        config = ''
+          lua << EOF
+          require('vscode').setup()
+          EOF
+        '';
+      }
     ];
-    extraLuaConfig = ''
-      require('bufferline').setup()
-      require('lualine').setup()
-      require('vscode').setup()
-      require('gitblame').setup()
-      require('git-conflict').setup()
-      require('gitsigns').setup()
-      require('mini.animate').setup()
-      require('mini.basics').setup()
-      require('mini.pairs').setup()
-      require('mini.trailspace').setup()
-    '';
-    extraConfig = ''
-      colorscheme vscode
-      set clipboard=unnamedplus
-      " Key bindings
-      nnoremap . :bn<CR>
-      nnoremap , :bp<CR>
-      " Delete char, don't cut
-      nnoremap x "_x
-      nnoremap X "_X
-      vnoremap x "_x
-      vnoremap X "_X
-      " Don't copy on paste
-      vnoremap p "_dP
-    '';
+    keymaps = [
+      {
+        mode = ["n"];
+        key = ".";
+        action = ":bn<CR>";
+        options = {
+          desc = "Next buffer";
+        };
+      }
+      {
+        mode = ["n"];
+        key = ",";
+        action = ":bp<CR>";
+        options = {
+          desc = "Previous buffer";
+        };
+      }
+      {
+        mode = ["n" "v"];
+        key = "x";
+        action = "\"_x";
+        options = {
+          desc = "Delete character, don't cut";
+        };
+      }
+      {
+        mode = ["n" "v"];
+        key = "X";
+        action = "\"_X";
+        options = {
+          desc = "Delete character, don't cut";
+        };
+      }
+      {
+        mode = ["v"];
+        key = "p";
+        action = "\"_dP";
+        options = {
+          desc = "Don' copy on paste";
+        };
+      }
+    ];
   };
 }
