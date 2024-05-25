488929ab467ab1d201cca444ef706f7040b2600e
Dawid Dziurla <dawidd0811@gmail.com>
2024-05-25 12:07:12 +0200

    devels: init

--- flake.nix
+++ flake.nix
@@ -71,6 +71,9 @@
           programs.alejandra.enable = true;
           programs.deadnix.enable = true;
           programs.statix.enable = true;
+          settings.global.excludes = [
+            "./devels/**"
+          ];
         };
       };
     };
