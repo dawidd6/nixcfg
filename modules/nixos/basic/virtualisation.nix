036d5b5c85850738da99eda818fc196d5edbb9c3
Dawid Dziurla <dawidd0811@gmail.com>
2024-05-26 20:48:59 +0200

    virtualisation: don't create disk image

--- modules/nixos/basic/virtualisation.nix
+++ modules/nixos/basic/virtualisation.nix
@@ -20,6 +20,7 @@
     virtualisation.cores = 4;
     virtualisation.memorySize = 4096;
     virtualisation.diskSize = 4096;
+    virtualisation.diskImage = null;
     virtualisation.graphics = config.services.xserver.enable;
     virtualisation.qemu.options = lib.mkIf config.services.xserver.enable [
       "-device virtio-vga"
