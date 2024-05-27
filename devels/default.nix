f3fa3d69e603a01f4854b185eb078b2c68f1ce60
Dawid Dziurla <dawidd0811@gmail.com>
2024-05-27 11:30:01 +0200

    devels: expect repos in HOME

--- devels/default.nix
+++ devels/default.nix
@@ -3,8 +3,8 @@
   username ? builtins.getEnv "USER",
   homedir ? builtins.getEnv "HOME",
   desktop ? true,
-  nixpkgs ? ./nixpkgs,
-  home-manager ? ./home-manager,
+  nixpkgs ? "${homedir}/nixpkgs",
+  home-manager ? "${homedir}/home-manager",
   ...
 }: rec {
   vmBuild = vmSystem.config.system.build.vm;
