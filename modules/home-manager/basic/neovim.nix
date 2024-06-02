75d7b3e691dffd331a27a877896824592701078f
Dawid Dziurla <dawidd0811@gmail.com>
2024-06-02 14:16:46 +0200

    neovim: better configuration

--- modules/home-manager/basic/neovim.nix
+++ modules/home-manager/basic/neovim.nix
@@ -6,19 +6,103 @@
     vimAlias = true;
     plugins = with pkgs.unstable.vimPlugins; [
       # Appearance
-      bufferline-nvim
-      lualine-nvim
-      noice-nvim
+      {
+        plugin = bufferline-nvim;
+        optional = true;
+        type = "lua";
+        config = ''
+          vim.cmd("packadd! bufferline.nvim")
+          require("bufferline").setup()
+        '';
+      }
+      {
+        plugin = lualine-nvim;
+        optional = true;
+        type = "lua";
+        config = ''
+          vim.cmd("packadd! lualine.nvim")
+          require("lualine").setup()
+        '';
+      }
+      {
+        plugin = noice-nvim;
+        optional = true;
+        type = "lua";
+        config = ''
+          vim.cmd("packadd! noice.nvim")
+          require("noice").setup()
+        '';
+      }
       # SCM
-      git-conflict-nvim
-      gitsigns-nvim
+      {
+        plugin = git-conflict-nvim;
+        optional = true;
+        type = "lua";
+        config = ''
+          vim.cmd("packadd! git-conflict.nvim")
+          require("git-conflict").setup()
+        '';
+      }
+      {
+        plugin = gitsigns-nvim;
+        optional = true;
+        type = "lua";
+        config = ''
+          vim.cmd("packadd! gitsigns.nvim")
+          require("gitsigns").setup()
+        '';
+      }
       # Convenience
-      guess-indent-nvim
-      nvim-autopairs
-      trim-nvim
-      which-key-nvim
+      {
+        plugin = guess-indent-nvim;
+        optional = true;
+        type = "lua";
+        config = ''
+          vim.cmd("packadd! guess-indent.nvim")
+          require("guess-indent").setup()
+        '';
+      }
+      {
+        plugin = nvim-autopairs;
+        optional = true;
+        type = "lua";
+        config = ''
+          vim.cmd("packadd! nvim-autopairs")
+          require("nvim-autopairs").setup()
+        '';
+      }
+      {
+        plugin = trim-nvim;
+        optional = true;
+        type = "lua";
+        config = ''
+          vim.cmd("packadd! trim.nvim")
+          require("trim").setup({
+            trim_on_write = false,
+            highlight = true,
+          })
+        '';
+      }
+      {
+        plugin = which-key-nvim;
+        optional = true;
+        type = "lua";
+        config = ''
+          vim.cmd("packadd! which-key.nvim")
+          require("which-key").setup()
+        '';
+      }
       # Colorscheme
-      vscode-nvim
+      {
+        plugin = vscode-nvim;
+        optional = true;
+        type = "lua";
+        config = ''
+          vim.cmd("packadd! vscode.nvim")
+          require("vscode").setup()
+          vim.cmd.colorscheme "vscode"
+        '';
+      }
     ];
     extraLuaConfig = ''
       vim.opt.clipboard = "unnamedplus"
@@ -29,45 +113,25 @@
       vim.keymap.set({"n","v"}, "X", "\"_X", { desc = "Don't cut character, just delete" })
       vim.keymap.set("v", "p", "\"_dP", { desc = "Don't copy on paste" })
 
-      if not vim.g.vscode then
-        vim.opt.expandtab = true
-        vim.opt.gdefault = true
-        vim.opt.ignorecase = true
-        vim.opt.number = true
-        vim.opt.shiftwidth = 4
-        vim.opt.showmatch = true
-        vim.opt.showtabline = 2
-        vim.opt.smartcase = true
-        vim.opt.smartindent = true
-        vim.opt.softtabstop = 4
-        vim.opt.swapfile = false
-        vim.opt.tabstop = 4
-        vim.opt.termguicolors = true
-        vim.opt.visualbell = true
-        vim.opt.writebackup = false
-
-        -- Appearance
-        require("bufferline").setup()
-        require("lualine").setup()
-        require("noice").setup()
-
-        -- SCM
-        require("git-conflict").setup()
-        require("gitsigns").setup()
-
-        -- Convenience
-        require("guess-indent").setup()
-        require("nvim-autopairs").setup()
-        require("trim").setup({
-          trim_on_write = false,
-          highlight = true,
-        })
-        require("which-key").setup()
-
-        -- Colorscheme
-        require("vscode").setup()
-        vim.cmd.colorscheme "vscode"
+      if vim.g.vscode then
+        return
       end
+
+      vim.opt.expandtab = true
+      vim.opt.gdefault = true
+      vim.opt.ignorecase = true
+      vim.opt.number = true
+      vim.opt.shiftwidth = 4
+      vim.opt.showmatch = true
+      vim.opt.showtabline = 2
+      vim.opt.smartcase = true
+      vim.opt.smartindent = true
+      vim.opt.softtabstop = 4
+      vim.opt.swapfile = false
+      vim.opt.tabstop = 4
+      vim.opt.termguicolors = true
+      vim.opt.visualbell = true
+      vim.opt.writebackup = false
     '';
   };
 }
