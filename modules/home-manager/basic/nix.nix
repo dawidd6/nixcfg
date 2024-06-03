afcebe081625936b534335e6e96e3e1fe8764e7b
Dawid Dziurla <dawidd0811@gmail.com>
2024-06-03 14:14:44 +0200

    nix: set package for standalone hm
    it is mandatory to set for standalone

--- modules/home-manager/basic/nix.nix
+++ modules/home-manager/basic/nix.nix
@@ -1,8 +1,9 @@
 {
   lib,
   inputs,
+  pkgs,
   ...
-}: {
+} @ args: {
   nix.registry = lib.mapAttrs (_: value: {flake = value;}) inputs;
 
   nix.gc.automatic = true;
@@ -11,6 +12,8 @@
 
   # https://github.com/nix-community/home-manager/issues/5465
   systemd.user.timers.nix-gc.Timer.Persistent = true;
+
+  nix.package = lib.mkIf (!args ? osConfig) pkgs.nix;
 
   nix.settings.extra-substituters = ["https://dawidd6.cachix.org"];
   nix.settings.extra-trusted-public-keys = ["dawidd6.cachix.org-1:dvy2Br48mAee39Yit5P+jLLIUR3gOa1ts4w4DTJw+XQ="];
