3d1c3e3295de8e0b03a5b878bd856882c38e91b8
Dawid Dziurla <dawidd0811@gmail.com>
2024-06-06 19:32:16 +0200

    apps: remove dconf settings

--- modules/home-manager/graphical/apps.nix
+++ modules/home-manager/graphical/apps.nix
@@ -20,24 +20,4 @@
     vscode
     yubioath-flutter
   ];
-
-  dconf.settings = {
-    "org/gnome/shell" = {
-      favorite-apps = [
-        "google-chrome.desktop"
-        "spotify.desktop"
-        "code.desktop"
-        "virt-manager.desktop"
-        "org.gnome.Nautilus.desktop"
-        "org.gnome.Console.desktop"
-      ];
-    };
-    "org/virt-manager/virt-manager" = {
-      system-tray = true;
-    };
-    "org/virt-manager/virt-manager/connections" = {
-      autoconnect = ["qemu:///system"];
-      uris = ["qemu:///system"];
-    };
-  };
 }
