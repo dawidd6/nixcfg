4e158fa557e45d19588f9d68ae64ab910ea10ef4
Dawid Dziurla <dawidd0811@gmail.com>
2024-06-21 15:38:46 +0200

    apps: use some from unstable

--- modules/home-manager/graphical/apps.nix
+++ modules/home-manager/graphical/apps.nix
@@ -1,26 +1,26 @@
 { pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    element-desktop
-    filezilla
-    firefox
-    fritzing
-    gimp
-    gnome.gnome-boxes
-    gnome.dconf-editor
-    gnome.gnome-tweaks
-    google-chrome
-    gpick
-    gscan2pdf
-    inkscape
-    keepassxc
-    krita
-    libreoffice
-    pavucontrol
-    signal-desktop
-    spotify
-    virt-manager
-    vscode
-    yubioath-flutter
-  ];
+  home.packages =
+    (with pkgs; [
+      filezilla
+      firefox
+      fritzing
+      gimp
+      gnome.gnome-boxes
+      gnome.dconf-editor
+      gnome.gnome-tweaks
+      google-chrome
+      gpick
+      gscan2pdf
+      inkscape
+      keepassxc
+      krita
+      libreoffice
+      pavucontrol
+      signal-desktop
+      spotify
+      virt-manager
+      yubioath-flutter
+    ])
+    ++ (with pkgs.unstable; [ vscode ]);
 }
