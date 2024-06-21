a2b97247cbfb06a1d07068ea39cf75654549dc16
Dawid Dziurla <dawidd0811@gmail.com>
2024-06-21 15:35:12 +0200

    tools: cleanup and use unstable for some

--- modules/home-manager/basic/tools.nix
+++ modules/home-manager/basic/tools.nix
@@ -2,56 +2,55 @@
 {
   imports = [ inputs.nix-index-database.hmModules.nix-index ];
 
-  home.packages = with pkgs; [
-    ansible
-    ansible-lint
-    btop
-    cloc
-    cpio
-    curl
-    deadnix
-    devbox
-    diffoscopeMinimal
-    distrobox
-    dos2unix
-    file
-    fx
-    gh
-    ghorg
-    glab
-    gnumake
-    htop
-    ipcalc
-    jq
-    lazygit
-    lm_sensors
-    ncdu
-    unstable.nil
-    unstable.nixfmt-rfc-style
-    nix-diff
-    nix-init
-    nix-tree
-    nix-update
-    nixos-shell
-    nixpkgs-review
-    nmap
-    nodejs
-    nurl
-    nvd
-    openstackclient
-    ruby
-    shellcheck
-    sshpass
-    strace
-    tealdeer
-    tmux
-    trash-cli
-    tree
-    unzip
-    wget
-    xsel
-    yubikey-manager
-  ];
+  home.packages =
+    (with pkgs; [
+      ansible
+      ansible-lint
+      btop
+      cloc
+      cpio
+      curl
+      diffoscopeMinimal
+      distrobox
+      dos2unix
+      file
+      fx
+      gh
+      ghorg
+      glab
+      gnumake
+      htop
+      ipcalc
+      jq
+      lm_sensors
+      ncdu
+      nmap
+      nodejs
+      nurl
+      ruby
+      shellcheck
+      sshpass
+      strace
+      tealdeer
+      tmux
+      trash-cli
+      tree
+      unzip
+      wget
+      xsel
+      yubikey-manager
+    ])
+    ++ (with pkgs.unstable; [
+      nil
+      nix-diff
+      nix-init
+      nix-tree
+      nix-update
+      nixfmt-rfc-style
+      nixos-shell
+      nixpkgs-review
+      nvd
+    ]);
 
   programs.fzf.enable = true;
   programs.less.enable = true;
