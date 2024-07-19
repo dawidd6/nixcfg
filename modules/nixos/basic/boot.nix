25ecf2ee2ff11fd5c96c4e2d005bd86aad9dd5c6
Dawid Dziurla <dawidd0811@gmail.com>
2024-07-19 22:35:07 +0200

    boot: 5 -> 10 configurations

--- modules/nixos/basic/boot.nix
+++ modules/nixos/basic/boot.nix
@@ -3,7 +3,7 @@
   boot.tmp.cleanOnBoot = true;
 
   boot.loader.systemd-boot.enable = true;
-  boot.loader.systemd-boot.configurationLimit = 5;
+  boot.loader.systemd-boot.configurationLimit = 10;
 
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = lib.mkIf (lib.versionOlder config.system.stateVersion "23.05") "/boot/efi";
