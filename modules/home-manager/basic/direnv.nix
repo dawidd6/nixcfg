97cc9532db4cee6869af75885e9366fab02e7282
Dawid Dziurla <dawidd0811@gmail.com>
2024-09-22 10:54:24 +0200

    direnv: disable some stuff

--- modules/home-manager/basic/direnv.nix
+++ modules/home-manager/basic/direnv.nix
@@ -1,4 +1,10 @@
 _: {
   programs.direnv.enable = true;
   programs.direnv.nix-direnv.enable = true;
+  programs.direnv.config = {
+    global = {
+      warn_timeout = "15m";
+      hide_env_diff = true;
+    };
+  };
 }
