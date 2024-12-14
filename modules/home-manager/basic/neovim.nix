a8151a754f54dea287b9bbb2bb45963f2c21bc4d
Dawid Dziurla <dawidd0811@gmail.com>
2024-12-15 00:24:15 +0100

    neovim: more configuration

--- modules/home-manager/basic/neovim.nix
+++ modules/home-manager/basic/neovim.nix
@@ -64,19 +64,24 @@
       }
     ];
     plugins = {
+      alpha = {
+        enable = true;
+        theme = "startify";
+      };
+      barbecue.enable = true;
       bufferline.enable = true;
       cmp = {
         enable = true;
         settings = {
           sources = [
             {
-              name = "async-path";
-            }
-            {
-              name = "cmdline";
-            }
-            {
               name = "buffer";
+            }
+            {
+              name = "nvim_lsp";
+            }
+            {
+              name = "path";
             }
           ];
           mapping = {
@@ -90,8 +95,46 @@
       git-conflict.enable = true;
       gitsigns.enable = true;
       guess-indent.enable = true;
+      lsp = {
+        enable = true;
+        servers = {
+          ansiblels = {
+            enable = true;
+          };
+          bashls = {
+            enable = true;
+          };
+          dartls = {
+            enable = true;
+          };
+          gopls = {
+            enable = true;
+          };
+          nixd = {
+            enable = true;
+          };
+          rubocop = {
+            enable = true;
+          };
+          ruby_lsp = {
+            enable = true;
+          };
+          ruff = {
+            enable = true;
+          };
+        };
+      };
       lualine.enable = true;
+      noice.enable = true;
       nvim-autopairs.enable = true;
+      rainbow-delimiters.enable = true;
+      treesitter = {
+        enable = true;
+        settings = {
+          highlight.enable = true;
+          indent.enable = true;
+        };
+      };
       trim.enable = true;
       web-devicons.enable = true;
       which-key.enable = true;
