fdc2500cd7e0ee26199f3a854d96314331e0c554
Dawid Dziurla <dawidd0811@gmail.com>
2024-12-17 12:36:41 +0100

    coruscant: allow interactive disko vm to boot

--- hosts/coruscant/disko-config.nix
+++ hosts/coruscant/disko-config.nix
@@ -1,30 +1,34 @@
 {
-  disko.devices = {
-    disk = {
-      main = {
-        type = "disk";
-        device = "/dev/nvme0n1";
-        content = {
-          type = "gpt";
-          partitions = {
-            boot = {
-              size = "500M";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-              };
-            };
-            root = {
-              size = "100%";
-              content = {
-                type = "luks";
-                name = "cryptroot";
+  disko = {
+    memSize = 4096;
+    devices = {
+      disk = {
+        main = {
+          imageSize = "64G";
+          type = "disk";
+          device = "/dev/nvme0n1";
+          content = {
+            type = "gpt";
+            partitions = {
+              boot = {
+                size = "500M";
+                type = "EF00";
                 content = {
                   type = "filesystem";
-                  format = "ext4";
-                  mountpoint = "/";
+                  format = "vfat";
+                  mountpoint = "/boot";
+                };
+              };
+              root = {
+                size = "100%";
+                content = {
+                  type = "luks";
+                  name = "cryptroot";
+                  content = {
+                    type = "filesystem";
+                    format = "ext4";
+                    mountpoint = "/";
+                  };
                 };
               };
             };
