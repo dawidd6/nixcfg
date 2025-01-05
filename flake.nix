36340ea1c869e8c284a87e07dc4e7938af48f6c1
Dawid Dziurla <dawidd0811@gmail.com>
2025-01-05 18:34:01 +0100

    flake: fix syntax

--- flake.nix
+++ flake.nix
@@ -56,7 +56,7 @@
       overlays.default = final: prev: {
         # https://github.com/NixOS/nixpkgs/pull/173364
         ansible = prev.ansible.overrideAttrs (oldAttrs: {
-          propagatedBuildInputs = oldAttrs.propagatedBuildInputs ++ final.python3Packages.jmespath;
+          propagatedBuildInputs = oldAttrs.propagatedBuildInputs ++ [ final.python3Packages.jmespath ];
         });
       };
 
