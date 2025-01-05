4967545db5ce3fd260187762393cc8a07784e672
Dawid Dziurla <dawidd0811@gmail.com>
2025-01-05 12:58:19 +0100

    extensions remove clipboard-indicator and user-themes

--- modules/home-manager/graphical/extensions.nix
+++ modules/home-manager/graphical/extensions.nix
@@ -1,18 +1,7 @@
 { pkgs, ... }:
-let
-  extensions = with pkgs.gnomeExtensions; [
-    appindicator
-    clipboard-indicator
-    user-themes
-  ];
-in
 {
-  home.packages = extensions;
-
-  dconf.settings = {
-    "org/gnome/shell" = {
-      enabled-extensions = map (extension: extension.extensionUuid) extensions;
-      disabled-extensions = [ ];
-    };
-  };
+  programs.gnome-shell.enable = true;
+  programs.gnome-shell.extensions = with pkgs.gnomeExtensions; [
+    { package = appindicator; }
+  ];
 }
