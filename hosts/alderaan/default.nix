515caa8823fed17d9a33c3f43b735446ea3077f9
dawidd6 <9713907+dawidd6@users.noreply.github.com>
2025-01-05 13:06:25 +0000

    flake: update

--- hosts/alderaan/default.nix
+++ hosts/alderaan/default.nix
@@ -27,9 +27,12 @@
   boot.initrd.secrets = {
     "/crypto_keyfile.bin" = null;
   };
-  boot.initrd.luks.devices."luks-4af00ef3-9f52-4447-9f2b-fcffedb33cde".device = "/dev/disk/by-uuid/4af00ef3-9f52-4447-9f2b-fcffedb33cde";
-  boot.initrd.luks.devices."luks-47d56822-d64c-4b0d-b454-b1cbf08d3b7c".device = "/dev/disk/by-uuid/47d56822-d64c-4b0d-b454-b1cbf08d3b7c";
-  boot.initrd.luks.devices."luks-47d56822-d64c-4b0d-b454-b1cbf08d3b7c".keyFile = "/crypto_keyfile.bin";
+  boot.initrd.luks.devices."luks-4af00ef3-9f52-4447-9f2b-fcffedb33cde".device =
+    "/dev/disk/by-uuid/4af00ef3-9f52-4447-9f2b-fcffedb33cde";
+  boot.initrd.luks.devices."luks-47d56822-d64c-4b0d-b454-b1cbf08d3b7c".device =
+    "/dev/disk/by-uuid/47d56822-d64c-4b0d-b454-b1cbf08d3b7c";
+  boot.initrd.luks.devices."luks-47d56822-d64c-4b0d-b454-b1cbf08d3b7c".keyFile =
+    "/crypto_keyfile.bin";
   boot.initrd.availableKernelModules = [
     "xhci_pci"
     "ehci_pci"
