737313bda3f8e4ce06934a1848697b1543817461
Dawid Dziurla <dawidd0811@gmail.com>
2025-01-08 10:04:53 +0100

    nixpkgs: comment out pkgsUnstable

--- modules/home-manager/basic/nixpkgs.nix
+++ modules/home-manager/basic/nixpkgs.nix
@@ -1,16 +1,14 @@
 {
-  inputs,
   outputs,
-  pkgs,
   lib,
   ...
 }@args:
 {
-  _module.args.pkgsUnstable = import inputs.nixpkgs-unstable {
-    inherit (pkgs) system;
-    config.allowUnfree = true;
-    overlays = [ outputs.overlays.default ];
-  };
+  #_module.args.pkgsUnstable = import inputs.nixpkgs-unstable {
+  #  inherit (pkgs) system;
+  #  config.allowUnfree = true;
+  #  overlays = [ outputs.overlays.default ];
+  #};
 
   nixpkgs = lib.mkIf (!args ? osConfig) {
     config.allowUnfree = true;
