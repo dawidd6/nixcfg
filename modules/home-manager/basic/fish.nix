7cd3c76e5fe6eeb3f642fc50f567ef31c83cf839
Dawid Dziurla <dawidd0811@gmail.com>
2025-01-09 22:13:36 +0100

    fish: remove a bunch of abbrs

--- modules/home-manager/basic/fish.nix
+++ modules/home-manager/basic/fish.nix
@@ -14,25 +14,11 @@
     generateCompletions = false;
     shellAliases = {
       rm = "trash";
-      ssh = "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=ERROR";
+      ssh = "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no";
       hub = "gh";
     };
     shellAbbrs = {
       e = "exit";
-      more = "less";
-      sl = "ls";
-      add = "sudo apt install";
-      purge = "sudo apt remove --purge";
-      autopurge = "sudo apt autoremove --purge";
-      update = "sudo apt update";
-      upgrade = "sudo apt full-upgrade";
-      search = "apt search";
-      clean = "sudo apt clean && sudo apt autoclean";
-      outdated = "apt list --upgradable";
-      belongs = "apt-file search";
-      contents = "apt-file list";
-      show = "apt show";
-      list = "dpkg -l";
       g = "git";
       ga = "git add";
       gc = "git commit -m";
@@ -43,19 +29,14 @@
       gd = "git diff";
       gdc = "git diff --cached";
       gh = "git checkout";
-      ghm = "git checkout (git symbolic-ref --short refs/remotes/origin/HEAD | sed 's@origin/@@')";
       gb = "git branch -a";
       gf = "git fetch";
       gp = "git push";
       gfo = "git fetch origin";
       gpo = "git push origin -u";
-      suspend = "systemctl suspend";
       clip = "xsel --clipboard";
       mic-test = "arecord -f cd - | aplay -";
-      docker = "podman";
-      d = "podman";
       p = "podman";
-      hm = "home-manager";
     };
     interactiveShellInit = ''
       set fish_color_command green
