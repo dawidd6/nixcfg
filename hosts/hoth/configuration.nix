624672519c256683b40df29e3d7dc94b74659870
Dawid Dziurla <dawidd0811@gmail.com>
2025-01-11 23:39:23 +0100

    hoth: set root ssh

--- hosts/hoth/configuration.nix
+++ hosts/hoth/configuration.nix
@@ -1,9 +1,9 @@
 {
   services.openssh.enable = true;
 
-  #users.users.root.openssh.authorizedKeys.keys = [
-  #  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII7iPyyt5SiB7irTbJHMPEJOWhCW+UBhPcNUjZnYAQyB dawidd6@coruscant"
-  #];
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII7iPyyt5SiB7irTbJHMPEJOWhCW+UBhPcNUjZnYAQyB dawidd6@coruscant"
+  ];
 
   swapDevices = [
     {
