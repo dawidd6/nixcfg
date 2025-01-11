4688f53b77f57d827891e912c09495df6b7a0a95
Dawid Dziurla <dawidd0811@gmail.com>
2025-01-11 19:26:32 +0100

    yavin: restructure

---
+++ hosts/yavin/configuration.nix
@@ -0,0 +1,8 @@
+{
+  networking.networkmanager.wifi.powersave = false;
+
+  services.openssh.enable = true;
+  services.udisks2.enable = true;
+
+  system.stateVersion = "22.11";
+}
--- hosts/yavin/default.nix
+++ hosts/yavin/default.nix
@@ -8,35 +8,12 @@
     inputs.hardware.nixosModules.common-cpu-intel
 
     outputs.nixosModules.basic
+
+    ./configuration.nix
+    ./hardware-configuration.nix
   ];
 
   home = {
     imports = [ outputs.homeModules.basic ];
   };
-
-  networking.networkmanager.wifi.powersave = false;
-
-  services.openssh.enable = true;
-  services.udisks2.enable = true;
-
-  boot.initrd.availableKernelModules = [
-    "ahci"
-    "xhci_pci"
-    "ums_realtek"
-    "uas"
-    "sd_mod"
-    "sdhci_pci"
-  ];
-  boot.kernelModules = [ "kvm-intel" ];
-
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/76fee5e5-78de-4203-af82-0dc3be4ad74b";
-    fsType = "ext4";
-  };
-  fileSystems."/boot/efi" = {
-    device = "/dev/disk/by-uuid/404E-91CE";
-    fsType = "vfat";
-  };
-
-  system.stateVersion = "22.11";
 }
