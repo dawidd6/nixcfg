c399aa2537000d55f28ed8c11687a133485715b4
Dawid Dziurla <dawidd0811@gmail.com>
2025-01-11 19:16:06 +0100

    users: factor out

--- users/dawid/default.nix
+++ users/dawid/default.nix
@@ -1,26 +1,8 @@
-{ outputs, pkgs, ... }:
+{ outputs, ... }:
 {
-  imports = [ outputs.homeModules.basic ];
+  imports = [
+    outputs.homeModules.basic
 
-  home.packages = [ pkgs.onedrive ];
-
-  systemd.user.services.onedrive = {
-    Unit.Description = "Onedrive sync service";
-    Install.WantedBy = [ "default.target" ];
-    Service = {
-      Environment = [
-        "Environment=HTTP_PROXY=http://10.144.1.10:8080"
-        "Environment=HTTPS_PROXY=http://10.144.1.10:8080"
-      ];
-      Type = "simple";
-      ExecStart = ''
-        ${pkgs.onedrive}/bin/onedrive --monitor --confdir=%h/.config/onedrive
-      '';
-      Restart = "on-failure";
-      RestartSec = 3;
-      RestartPreventExitStatus = 3;
-    };
-  };
-
-  home.stateVersion = "22.11";
+    ./home.nix
+  ];
 }
---
+++ users/dawid/home.nix
@@ -0,0 +1,24 @@
+{ pkgs, ... }:
+{
+  home.packages = [ pkgs.onedrive ];
+
+  systemd.user.services.onedrive = {
+    Unit.Description = "Onedrive sync service";
+    Install.WantedBy = [ "default.target" ];
+    Service = {
+      Environment = [
+        "Environment=HTTP_PROXY=http://10.144.1.10:8080"
+        "Environment=HTTPS_PROXY=http://10.144.1.10:8080"
+      ];
+      Type = "simple";
+      ExecStart = ''
+        ${pkgs.onedrive}/bin/onedrive --monitor --confdir=%h/.config/onedrive
+      '';
+      Restart = "on-failure";
+      RestartSec = 3;
+      RestartPreventExitStatus = 3;
+    };
+  };
+
+  home.stateVersion = "22.11";
+}
