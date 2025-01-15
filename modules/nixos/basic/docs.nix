8530dfe96524259900cbc0eb6c85e4402fb2ed18
Dawid Dziurla <dawidd0811@gmail.com>
2025-01-15 15:01:26 +0100

    basic: move generateCaches from docs to fish

--- modules/nixos/basic/docs.nix
+++ modules/nixos/basic/docs.nix
@@ -2,5 +2,4 @@ _: {
   documentation.doc.enable = false;
   documentation.info.enable = false;
   documentation.nixos.enable = false;
-  documentation.man.generateCaches = false;
 }
--- modules/nixos/basic/fish.nix
+++ modules/nixos/basic/fish.nix
@@ -8,6 +8,8 @@
   # TODO: upstream this (disable completions generation)
   environment.etc."fish/generated_completions".text = lib.mkForce "";
 
+  documentation.man.generateCaches = false;
+
   programs.fish.enable = true;
   programs.fish.useBabelfish = true;
 
