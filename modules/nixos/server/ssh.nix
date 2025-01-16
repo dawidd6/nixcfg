d069889e571fa68226e6635aa870ad8aa47a8483
Dawid Dziurla <dawidd0811@gmail.com>
2025-01-16 16:04:24 +0100

    ssh: fix

--- modules/nixos/server/ssh.nix
+++ modules/nixos/server/ssh.nix
@@ -3,7 +3,7 @@
   services.openssh = {
     enable = true;
     settings = {
-      PermitRootLogin = false;
+      PermitRootLogin = "no";
       PasswordAuthentication = false;
       KbdInteractiveAuthentication = false;
       AllowUsers = [ userName ];
