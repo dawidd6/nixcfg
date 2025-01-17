a91003203dd7f3703e593cca03f6829f0c9d1c29
Dawid Dziurla <dawidd0811@gmail.com>
2025-01-17 08:38:13 +0100

    server/ssh: add work laptop pubkey

--- modules/nixos/server/ssh.nix
+++ modules/nixos/server/ssh.nix
@@ -12,5 +12,6 @@
 
   users.users."${userName}".openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII7iPyyt5SiB7irTbJHMPEJOWhCW+UBhPcNUjZnYAQyB dawidd6@coruscant"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHFYlpP/NkexiW9B26EnMS6JLE/D35iu7KbKO1eFOcsr dawid@ThinkPad-T14-Gen-1"
   ];
 }
