b11d7392c08208f029901256d5bb69a9caf9f73e
Dawid Dziurla <dawidd0811@gmail.com>
2025-01-18 17:40:54 +0100

    ssh: add phone key

--- modules/nixos/server/ssh.nix
+++ modules/nixos/server/ssh.nix
@@ -13,5 +13,6 @@
   users.users."${userName}".openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII7iPyyt5SiB7irTbJHMPEJOWhCW+UBhPcNUjZnYAQyB dawidd6@coruscant"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHFYlpP/NkexiW9B26EnMS6JLE/D35iu7KbKO1eFOcsr dawid@ThinkPad-T14-Gen-1"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBg5okX2Lmqk/kj9kXW/h3D5u0+6bO7jtdd8U4/NT3FE phone"
   ];
 }
