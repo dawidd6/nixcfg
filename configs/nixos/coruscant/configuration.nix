09eb91780958b9e6aeb6c2c9d457c3af3ecafe86
Dawid Dziurla <dawidd0811@gmail.com>
2025-01-19 15:56:33 +0100

    coruscant: sleep on lidswitch for now

--- configs/nixos/coruscant/configuration.nix
+++ configs/nixos/coruscant/configuration.nix
@@ -23,7 +23,7 @@
   services.fprintd.tod.enable = true;
   services.fprintd.tod.driver = pkgs.libfprint-2-tod1-vfs0090;
 
-  services.logind.lidSwitch = "hibernate";
+  #services.logind.lidSwitch = "hibernate";
 
   boot.binfmt.emulatedSystems = [
     "armv7l-linux"
