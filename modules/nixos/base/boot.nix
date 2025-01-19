3e7c8a6c2d730ab33155b65809b8e664e764ba0e
Dawid Dziurla <dawidd0811@gmail.com>
2025-01-19 13:23:50 +0100

    boot: 10 -> 5 config limit

--- modules/nixos/base/boot.nix
+++ modules/nixos/base/boot.nix
@@ -1,6 +1,6 @@
 _: {
   boot.tmp.cleanOnBoot = true;
   boot.loader.systemd-boot.enable = true;
-  boot.loader.systemd-boot.configurationLimit = 10;
+  boot.loader.systemd-boot.configurationLimit = 5;
   boot.loader.efi.canTouchEfiVariables = true;
 }
