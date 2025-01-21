89a843e4824ce8144490cd202f5571fd8e4d4867
Dawid Dziurla <dawidd0811@gmail.com>
2025-01-21 16:16:55 +0100

    hoth: remove root openssh keys from main config

--- configs/nixos/hoth/configuration.nix
+++ configs/nixos/hoth/configuration.nix
@@ -9,11 +9,6 @@
 
   home-manager.users.${userName} = ./home.nix;
 
-  # TODO: check if we can connect as non root
-  #users.users.root.openssh.authorizedKeys.keys = [
-  #  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII7iPyyt5SiB7irTbJHMPEJOWhCW+UBhPcNUjZnYAQyB dawidd6@coruscant"
-  #];
-
   swapDevices = [
     {
       device = "/swap";
