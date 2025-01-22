2c13c052c16c66e7f75af1f6bf05ce83f72ed3c4
Dawid Dziurla <dawidd0811@gmail.com>
2025-01-22 16:50:39 +0100

    hoth: annotate bootstrap configuration

--- configs/nixos/hoth/bootstrap-configuration.nix
+++ configs/nixos/hoth/bootstrap-configuration.nix
@@ -1,3 +1,10 @@
+# This is a special configuration for bootstrapping of this host.
+# Oracle Cloud free tier only offers 1GB x86_64 machines.
+# NixOS installation via nixos-anywhere builds system closure
+# and stores it in RAM, which we have not many.
+# That's why this minimal configuration exists.
+# We deploy NixOS via nixos-anywhere using this configuration
+# and then switch to the main configuration no problem.
 { inputs, modulesPath, ... }:
 {
   imports = [
