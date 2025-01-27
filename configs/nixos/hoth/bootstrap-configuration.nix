f5d64dc0d08895aa4e9e69329514f12b8e92f7f8
Dawid Dziurla <dawidd0811@gmail.com>
2025-01-27 11:16:05 +0100

    flake: cleanup ssh keys

--- configs/nixos/hoth/bootstrap-configuration.nix
+++ configs/nixos/hoth/bootstrap-configuration.nix
@@ -21,8 +21,6 @@
   services.openssh.enable = true;
 
   users.users.root.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII7iPyyt5SiB7irTbJHMPEJOWhCW+UBhPcNUjZnYAQyB dawidd6@coruscant"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHFYlpP/NkexiW9B26EnMS6JLE/D35iu7KbKO1eFOcsr dawid@ThinkPad-T14-Gen-1"
   ];
 
   nixpkgs.hostPlatform = "x86_64-linux";
