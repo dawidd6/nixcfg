fa7a56abfdae962907ef9d5db7ca9e5e95f96017
Dawid Dziurla <dawidd0811@gmail.com>
2025-01-29 19:09:34 +0100

    flake: autostart 1password

--- flake.nix
+++ flake.nix
@@ -96,6 +96,16 @@
         ansible = prev.ansible.overrideAttrs (oldAttrs: {
           propagatedBuildInputs = oldAttrs.propagatedBuildInputs ++ [ final.python3Packages.jmespath ];
         });
+        _1password-gui = prev._1password-gui.overrideAttrs (oldAttrs: {
+          installPhase =
+            oldAttrs.installPhase
+            + ''
+              mkdir -p $out/etc/xdg/autostart
+              cp $out/share/applications/1password.desktop $out/etc/xdg/autostart/1password.desktop
+              substituteInPlace $out/etc/xdg/autostart/1password.desktop \
+                --replace-fail 'Exec=1password %U' 'Exec=1password --silent %U'
+            '';
+        });
       };
 
       nixosConfigurations = {
