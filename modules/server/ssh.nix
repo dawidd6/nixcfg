ccea4ec280f42e6236c96a2c71bc084a70464699
Dawid Dziurla <dawidd0811@gmail.com>
2025-01-30 17:30:23 +0100

    ssh: add work key
    [ci skip]

--- modules/server/ssh.nix
+++ modules/server/ssh.nix
@@ -12,6 +12,7 @@ mkModule {
     };
 
     users.users."${userName}".openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPQhxjhpM/MS4M7qOT1rt+gopvvUV56I6/IOHwgfqtvF dawid@work"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII7iPyyt5SiB7irTbJHMPEJOWhCW+UBhPcNUjZnYAQyB dawidd6@coruscant"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBg5okX2Lmqk/kj9kXW/h3D5u0+6bO7jtdd8U4/NT3FE phone"
     ];
